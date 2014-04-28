c6b202ee3c3db70ac56c226bb1af458219ba301c
Sebastian Quack <sebastianquack@gmail.com>
2014-04-28 13:49:08 +0200

    add comments

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,22 +1,47 @@
+/* variable declarations */ 
+
+var socket
+
+/* function declarations */
+
+// type command in input field and submit to server
+function autoType(text) {
+  $('#input-command').focus()
+  $('#input-command').val(text)
+  setTimeout(submitCommand, 800)
+}
+
+// submit a command to the server and clear input field 
+function submitCommand() {
+  val = $('#input-command').val()
+  socket.emit('chat-submit', { uuid: $.cookie('city54_uuid'), value: val })
+  $('#input-command').val('')
+}
+
+/* let's go! */
 
 $(document).ready(function() {
+
+  // set up sockets
+  socket = io.connect(window.location.origin)
+  console.log(window.location.origin)
   
-  console.log('ready')  
-  
-  // cookies
+  // check for cookies
   if($.cookie('city54_uuid')) {
     console.log('uuid found: ' + $.cookie('city54_uuid'))
   } else {
     $.cookie('city54_uuid', uuid.v1())
     console.log('uuid set to' + $.cookie('city54_uuid'))
   }
-  
-  // sockets
-  var socket = io.connect(window.location.origin)
-  console.log(window.location.origin)
-
+  // send cookie to server for check 
   socket.emit('uuid-check', { uuid: $.cookie('city54_uuid') })
 
+  // focus input field
+  $('#input-command').focus()
+
+  /* events */
+
+  // a chat item comes in from the server
   socket.on('chat-update', function (data) {
     if(data.player_name == "System") {
       newElem = $('<li>' + data.value + '</li>')
@@ -24,36 +49,25 @@ $(document).ready(function() {
       newElem = $('<li>' + data.player_name + ': ' + data.value + '</li>')
     }
     $('ul#chat').append(newElem)
-	$('ul#chat').animate({
-		scrollTop: $("ul#chat")[0].scrollHeight - $("ul#chat").innerHeight()
-		}, {
-	    duration: 300,//$("ul#chat li:last-child").height()*10,
-	    queue: false,
-	    easing: "swing"
-	    })
+	  $('ul#chat').animate( {
+		    scrollTop: $("ul#chat")[0].scrollHeight - $("ul#chat").innerHeight()
+      }, {
+	      duration: 300,//$("ul#chat li:last-child").height()*10,
+        queue: false,
+        easing: "swing"
+      })
+    })
+    
+  // user clicks on commands
+  $("body").on("click","b[data-command]", null, function() { 
+    autoType($(this).data("command")) 
   })
-  
-  // focus input field
-  $('#input-command').focus()
-  
-  // make commands clickable
-  $("body").on("click","b[data-command]",null, function() { autoType( $(this).data("command") ) } )
 
-  // user input events
+  // user hits enter in console
   $('#input-command').on("keypress", function(e) {
-    if (e.keyCode == 13) { submitCommand() }
+    if (e.keyCode == 13) { 
+      submitCommand() 
+    }
   })
-
-  function autoType(text) {
-    $('#input-command').focus()
-    $('#input-command').val(text)
-    setTimeout(submitCommand, 800)
-  }
-
-  function submitCommand() {
-    val = $('#input-command').val()
-    socket.emit('chat-submit', { uuid: $.cookie('city54_uuid'), value: val })
-    $('#input-command').val('')
-  }
-
+  
 })
