7ac616d2687cca4f94d5bbff05f9982ab03d60a6
Sebastian Quack <sebastianquack@gmail.com>
2014-05-09 20:08:55 +0200

    change title

--- config/routes.js
+++ config/routes.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
 
   // render main app
   app.get('/', function (req, res) {
-    res.render('home', {title: 'Foo'})
+    res.render('home', {title: '54. Stadt'})
   })
 
 }
