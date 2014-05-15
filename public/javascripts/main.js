11e5f5c55e4331119e224df5697e84350d9cd874
retani <holgheiss@gmail.com>
2014-05-15 15:21:44 +0200

    cool cursor

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -16,7 +16,24 @@ function submitCommand() {
   val = $('#input-command').val()
   socket.emit('player-action', { uuid: $.cookie('city54_uuid'), input: val })
   $('#input-command').val('')
+  updateInput()
 }
+
+// fill fake input with text, set cursor element
+splitCursor = function (text, position) {
+  begin = text.substr(0,position)
+  cursor = text.substr(position,1)
+  end = text.substr(position+1) + " "
+  return '<span>'+begin+'</span><span id="cursor">'+cursor+'</span><span>'+end+'</span>'
+}
+
+// update fake input
+updateInput = function() {
+    $('#input-fake').html(splitCursor($('#input-command').val(), $('#input-command').getCursorPosition()))
+    $('#input-command').focus()
+    if ($('#input-command').val().length >= 1) $('#input').addClass('chars')
+    else $('#input').removeClass('chars')
+  }
 
 /* let's go! */
 
@@ -34,12 +51,6 @@ $(document).ready(function() {
   }
   // send cookie to server for check 
   socket.emit('player-action', { uuid: $.cookie('city54_uuid'), firstPlayerAction: true })
-
-  // focus input field
-  $('#input-command').focus()
-  $('body').on("keypress keyup keydown click focus resize", function() {
-    $('#input-command').focus()
-  })
 
   /* events */
 
@@ -61,26 +72,32 @@ $(document).ready(function() {
 
     // todo: check what is going on
     if(data.sender_name == "System") {
-      newElem = $('<p data-sender="system">' + data.value + '</p>')
+      newElem = $('<p>' + data.value + '</p>')
     } else {
-      newElem = $('<p data-sender="player"><i>' + data.sender_name + ':</i> ' + data.value + '</p>')
+      newElem = $('<p data-sender="'+data.sender_name+'">' + data.value + '</p>')
     }
     if (data.type != undefined) newElem = newElem.addClass(data.type)
     $('#chat section:last-child').append(newElem)
   
-    $('#chat section:last-child').append($("#input-command").detach())
+    // move input field to bottom and update data
+    $('#chat section:last-child').append($("#input").detach())
+    $("#input").attr("data-sender",player.name)
+    $("#input").attr("data-state",player.state)
     $("#input-command").focus()
 
     // scroll up to fit new item
 	  $('#chat').animate( {
 		    scrollTop: $("#chat")[0].scrollHeight - $("#chat").innerHeight()
       }, {
-	      duration: 300,//$("ul#chat li:last-child").height()*10,
+	      duration: 600, //$("ul#chat p:last-child").height()*100,
         queue: false,
         easing: "swing"
       })
   })
     
+  // focus input field
+  $('body').on("keypress keyup keydown click focus resize touchstart touchleave load", updateInput)
+
   // user clicks on commands
   $("body").on("click","b[data-command]", null, function() { 
     autoType($(this).data("command")) 
@@ -93,24 +110,39 @@ $(document).ready(function() {
     }
   })
 
-  //$('body').keydown(function(e) {keyboard(e)})
-  
+  // Initialize jRumble on Selector
+  $('body').jrumble();
+
+  /*
+  // Start rumble on element
+  $('body').trigger('startRumble');
+
+  // Stop rumble on element
+  $('body').trigger('stopRumble');
+  */
+
+  // blink cursor
+  setInterval(function(){ $("#cursor").toggleClass("inverted")}, 800);
+
+  // detect cursor position in <input>
+  (function($) {
+      $.fn.getCursorPosition = function() {
+          var input = this.get(0);
+          if (!input) return; // No (input) element found
+          if ('selectionStart' in input) {
+              // Standard-compliant browsers
+              return input.selectionStart;
+          } else if (document.selection) {
+              // IE
+              input.focus();
+              var sel = document.selection.createRange();
+              var selLen = document.selection.createRange().text.length;
+              sel.moveStart('character', -input.value.length);
+              return sel.text.length - selLen;
+          }
+      }
+  })(jQuery);
+
 })
 
-/*
-var keyboard = function(e) {
-    var code = (e.keyCode ? e.keyCode : e.which);
-    // Enter key?
-    if(code == 13)
-    {
-        // Don't put a newline if this is the first command
-        if ($('#PastCommands').html() != '')
-            $('#PastCommands').append('<br />');
-        $('#PastCommands').append($(this).val());
-        $(this).val('');
-    }
-    else
-        $('#input-command').html($(this).val());
-}
-*/
 
