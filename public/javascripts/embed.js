1c39c4c3e8c07a4ee8c6b0c965075d6b810fc108
Sebastian Quack <sebastianquack@gmail.com>
2014-05-23 12:15:42 +0200

    embed size tool

--- public/javascripts/embed.js
+++ public/javascripts/embed.js
@@ -1,9 +1,29 @@
-
+var ifrm = null
 /* let's go! */
 
 $(document).ready(function() {
 
-  alert('embed tool')
+  $('#width').val(200) // default values
+  $('#height').val(400) // default values
+
+  $('#submit').on('click', function(event) {
+
+    if(ifrm) {
+      ifrm.parentNode.removeChild(ifrm)
+      ifrm = null  
+    }
+    
+    // create the iframe
+    ifrm = document.createElement("iframe")
+    ifrm.setAttribute("src", "http://city54.herokuapp.com")
+    ifrm.frameBorder = "0"
+    ifrm.style.width = $('#width').val()+"px"
+    ifrm.style.height = $('#height').val()+"px"
+    document.body.appendChild(ifrm)
+    
+    var text = document.createTextNode(ifrm.outerHTML);    
+    $('#embed-code').html('').append(text);        
+  })
 
 })
 
