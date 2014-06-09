c87ea152c86bcb4467b4e851496cc231054d9b52
Sebastian Quack <sebastianquack@gmail.com>
2014-06-10 01:08:41 +0200

    bot update

--- app/bots/basic_bot.js
+++ app/bots/basic_bot.js
@@ -55,15 +55,11 @@ var handleInput = function(bot, player, input, callback, prefix) {
       output.answer = "Schön dich wiederzusehen, " + 
       Util.capitaliseFirstLetter(bot.playerInfo[player.uuid].playerName) + "! Und, was gibt's Neues?"
       bot.setState(player, "check_keyphrase")
-      // todo: ask how quest is going if there is one?         
       break
       
     case "check_keyphrase":
       var quests = player.getActiveQuestsToBot(bot.name) // get all active quests leading to this bot
-
       if(quests.length > 0) {
-
-        bot.setState(player, "ask_content") //default option, in case nothing matches
         
         var messages = []
         quests.forEach(function(quest) {
@@ -91,11 +87,13 @@ var handleInput = function(bot, player, input, callback, prefix) {
           if(bestMatchIndex != -1) { 
             bot.playerInfo[player.uuid].currentQuest = quests[bestMatchIndex] // save the current quest
             bot.setState(player, "ask_sender_confirmation")
-          } 
+          } else {
+            bot.setState(player, "offer_quest")
+          }
         }
         
       } else { // there is no quest leading to this bot
-        bot.setState(player, "offer_quest") // all is set to create a relationship and send user on a quest
+        bot.setState(player, "offer_quest")
       } 
       handleInput(bot, player, input, callback) 
       return
@@ -130,7 +128,7 @@ var handleInput = function(bot, player, input, callback, prefix) {
         
         player.resolveQuest(bot.playerInfo[player.uuid].currentQuest)
         bot.playerInfo[player.uuid].currentQuest = null
-        // todo: update relationship
+        // todo: create/update relationship
                 
         bot.setState(player, "offer_quest")
         handleInput(bot, player, input, callback, prefix)
@@ -139,7 +137,7 @@ var handleInput = function(bot, player, input, callback, prefix) {
       else if(input.search(RegexNo) != -1) {
         // todo: respond to message type
         prefix = "Ach soo... dann ist das ja nicht so wichtig."
-        bot.setState(player, "ask_content")
+        bot.setState(player, "offer_quest")
         handleInput(bot, player, input, callback, prefix)
         return
       }
@@ -150,19 +148,44 @@ var handleInput = function(bot, player, input, callback, prefix) {
       break
       
     case "ask_content":
-      // todo: ask for different message types
+      
+      if(bot.relationships.length > 0) { // bot already has a relationship, todo: define minimum number of relationships
+        // pick a random bot, todo: branch on relationship status
+        var randomRelationship = bot.relationships[Math.floor(Math.random() * bot.relationships.length)]
+        output.answer = "Hm... wie es wohl " + randomRelationship.bot + " in " + randomRelationship.place + " geht... "        
+      } else {
+        bot.findRandomOtherBot(function(randomOtherBot) {                    
+          if(randomOtherBot) {
+            var newRelationship = {
+              bot: randomOtherBot.name, 
+              place: Util.capitaliseFirstLetter(randomOtherBot.room.split('/')[0]), 
+              level: 1 
+            }
+            bot.relationships.push(newRelationship)          
+
+            bot.setState(player, "offer_quest")
+            handleInput(bot, player, input, callback, prefix)
+        
+          } else {
+            bot.setState(player, "ask_content") // in case no other bot was found
+            handleInput(bot, player, input, callback, prefix)
+          }
+        }) 
+        return
+      }
+      
       switch(Math.floor(Math.random() * 4)) {
         case 0: 
-          output.answer = "Sag mal, kennst du lustige Witze? Kannst du mir einen sagen?"
+          output.answer += "Sag mal, kennst du lustige Witze? Kannst du mir einen sagen?"
           bot.playerInfo[player.uuid].currentMessageType = 'witz'
           break
-        case 1: output.answer = "Sag mal, kennst du sexy Anmachsprüche? Kannst du mir einen sagen?"
+        case 1: output.answer += "Sag mal, kennst du sexy Anmachsprüche? Kannst du mir einen sagen?"
           bot.playerInfo[player.uuid].currentMessageType = 'anmachspruch'
           break
-        case 2: output.answer = "Sag mal, kennst du fiese Beleidigungen? Kannst du mir eine sagen?"
+        case 2: output.answer += "Sag mal, kennst du fiese Beleidigungen? Kannst du mir eine sagen?"
           bot.playerInfo[player.uuid].currentMessageType = 'beleidigung'
           break
-        default: output.answer = "Sag mal, kennst du romantische Komplimente? Kannst du mir eins sagen?"
+        default: output.answer += "Sag mal, kennst du romantische Komplimente? Kannst du mir eins sagen?"
           bot.playerInfo[player.uuid].currentMessageType = 'kompliment'
       }
       bot.setState(player, "get_content")
@@ -186,7 +209,7 @@ var handleInput = function(bot, player, input, callback, prefix) {
           type: bot.playerInfo[player.uuid].currentMessageType,
           author: player.name
         })
-        prefix = "Danke!"
+        prefix += "Danke!"
         bot.setState(player, "offer_quest")        
         handleInput(bot, player, input, callback, prefix)
         return
@@ -195,16 +218,19 @@ var handleInput = function(bot, player, input, callback, prefix) {
       
     case "offer_quest":
 
+      activeQuests = player.getActiveQuestsFromBot(bot.name)
+
       if(!bot.globalVariables.messages) bot.globalVariables.messages = [] 
-      if(bot.globalVariables.messages.length < 4) { 
+      if(bot.globalVariables.messages.length < 2 || Math.random() < 0.5) { 
 
         bot.setState(player, "ask_content") 
         handleInput(bot, player, input, callback, prefix)
         return
 
-      } else if(player.getActiveQuestsFromBot(bot.name).length > 0) {
+      } else if(activeQuests.length > 0) {
         
-        output.answer = "Ich hoffe du hast meinen Auftrag nicht vergessen... Tschüss!"
+        output.answer = "Ich hoffe du hast meine Bitte nicht vergessen... Ich hatte dich ja vor kurzem gefragt, ob du " + Util.capitaliseFirstLetter(activeQuests[0].toBot) + " in " + activeQuests[0].toPlace + " von mir sagen könntest: '" + activeQuests[0].message.text + "'. Wär' echt toll wenn du das für mich machen könntest.<br>"
+"Tschüss!"
         output.abort = 'bot'
         
       } else {
@@ -229,38 +255,9 @@ var handleInput = function(bot, player, input, callback, prefix) {
           bot.setState(player, "get_quest_confirmation")
           output.answer = "Hey, kannst du nach " + randomRelationship.place + " gehen und " + Util.capitaliseFirstLetter(randomRelationship.bot) + " eine Nachricht von mir übermitteln?"
 
-        } else { // bot needs to create a relationship first        
-          // pick a random bot
-          bot.findRandomOtherBot(function(randomOtherBot) {                    
-            if(randomOtherBot) {
-              var newRelationship = {
-                bot: randomOtherBot.name, 
-                place: Util.capitaliseFirstLetter(randomOtherBot.room.split('/')[0]), 
-                level: 1 
-              }
-              bot.relationships.push(newRelationship)          
-              bot.playerInfo[player.uuid].currentQuest = {
-                questGiver: bot.name, 
-                questGiverPlace: Util.capitaliseFirstLetter(bot.room.split('/')[0]), 
-                fromBot: bot.name, 
-                fromPlace: Util.capitaliseFirstLetter(bot.room.split('/')[0]), 
-                toBot: newRelationship.bot, 
-                toPlace: newRelationship.place,
-                message: randomMessage, 
-                status: 'active'
-              }
-
-              bot.setState(player, "get_quest_confirmation")
-              output.answer = "Hey, kannst du nach " + newRelationship.place + " gehen und " + Util.capitaliseFirstLetter(randomOtherBot.name) + " eine Nachricht von mir übermitteln?"
-              output.bot = bot // pass updated bot into callback
-              callback(output)
-            
-            } else {
-              bot.setState(player, "ask_content") // in case no other bot was found
-              handleInput(bot, player, input, callback, prefix)
-            }
-          }) 
-          return
+        } else { // bot needs to create a relationship first, shouldn't happen, as relationships are create with content        
+          bot.setState(player, "ask_content") 
+          handleInput(bot, player, input, callback, prefix)
         }
       }
       
